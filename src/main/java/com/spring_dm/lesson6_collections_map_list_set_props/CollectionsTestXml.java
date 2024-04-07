90719a70e18a4400bcadcb2cd18436472912dd4b
Kravtsev Artem <arkravt@gmail.com>
2024-04-07 19:51:39 +0400

    07.04.2024

---
+++ src/main/java/com/spring_dm/lesson6_collections_map_list_set_props/CollectionsTestXml.java
@@ -0,0 +1,52 @@
+package com.spring_dm.lesson6_collections_map_list_set_props;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+public class CollectionsTestXml {
+
+    private Map<String, String> map;
+    private Properties props;
+    private Set<String> set;
+    private List<String> list;
+
+    public Map<String, String> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, String> map) {
+        this.map = map;
+    }
+
+    public Properties getProps() {
+        return props;
+    }
+
+    public void setProps(Properties props) {
+        this.props = props;
+    }
+
+    public Set<String> getSet() {
+        return set;
+    }
+
+    public void setSet(Set<String> set) {
+        this.set = set;
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+
+    public void displayInfo() {
+
+
+
+    }
+}
---
+++ src/main/java/com/spring_dm/lesson6_collections_map_list_set_props/Main.java
@@ -0,0 +1,4 @@
+package com.spring_dm.lesson6_collections_map_list_set_props;
+
+public class Main {
+}
